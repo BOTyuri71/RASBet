ffc18a1d471d51b4694083e24a322d4f6ca7c4d8
BOTyuri71 <gui.araujo5656@gmail.com>
2022-11-04 18:10:23 +0000

    UPDATE FRONT - header,login

--- front_end/src/pages/register/RegisterPage.js
+++ front_end/src/pages/register/RegisterPage.js
@@ -2,20 +2,23 @@ import React, { useState } from 'react';
 import img_reg from '../../assets/form_register.jpg'
 import DatePicker from 'react-date-picker';
 import {Controller,useForm} from 'react-hook-form';
-import {Register,Col1,Img1,Div1,Section1,Form1,Input1,Button1} from '../../styles/register'
+import {Register,Col1,Img1,Div1,Section1,Form1,Input1,Button1} from '../../styles/form'
+import Header from '../../components/header/Header';
 
 export default function Form() {
     const [value, onChange] = useState(new Date());
     const {control,register,handleSubmit} = useForm()
     const onSubmit = data => console.log(data);
   return (
+    <div>
+    <Header/>
     <Section1>
         <Div1>
         <Register>
         <Col1>
             <h2>Registo</h2>
 
-            <Form1 className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
+            <Form1 onSubmit={handleSubmit(onSubmit)}>
                 <Input1 type="text" {...register("email")} placeholder='E-mail'></Input1>
                 <Input1 type="password" {...register("password")} placeholder='Palavra-passe'></Input1>
                 <Controller
@@ -39,5 +42,6 @@ export default function Form() {
         </Register>
         </Div1>
     </Section1>
+    </div>
   )
 }
---
+++ front_end/src/styles/header.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Nav = styled.nav`
+    height: 50px;
+    border: 1px solid #dfdfdf;
+`;
+
+export const Header1 = styled.div`
+    width: 80%;
+    height: 100%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const H2 = styled.h2`
+    padding-left: 10px;
+`;
