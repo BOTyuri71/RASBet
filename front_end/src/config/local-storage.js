2e963806e2dbee50577fb9d22fd8846532e7cb64
Eduardo Araujo <eduardojafaraujo@gmail.com>
2023-01-16 23:00:18 +0000

    aadad

--- front_end/src/config/local-storage.js
+++
@@ -1,9 +0,0 @@
-export async function getItem() {
-  const value = await localStorage.getItem('userToken');
-}
-export async function setItem(value) {
-  return localStorage.setItem('userToken', JSON.stringify(value));
-}
-export async function removeItem() {
-  return localStorage.removeItem('userToken');
-}
